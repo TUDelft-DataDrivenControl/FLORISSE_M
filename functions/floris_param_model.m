0fe09ef181c0f5ba54c1899d1ab0e74d44f49647
Bartdoekemeijer <B.M.Doekemeijer@tudelft.nl>
2016-11-02 15:49:30 +0100

    Remove unavailable options

--- functions/floris_param_model.m
+++ functions/floris_param_model.m
@@ -3,7 +3,7 @@ function [ model ] = floris_parameters( model )
         case 'default' %% original tuning parameters
         model.pP              = 1.88; % yaw power correction parameter
         model.ke              = 0.05; % wake expansion parameters
-        model.keCorrArray     = 0.0; % array-correction factor: NOT YET IMPLEMENTED!
+        % model.keCorrArray     = 0.0; % array-correction factor: NOT YET IMPLEMENTED!
         model.keCorrCT        = 0.0; % CT-correction factor
         model.baselineCT      = 4.0*(1.0/3.0)*(1.0-(1.0/3.0)); % Baseline CT for ke-correction
         model.me              = [-0.5, 0.22, 1.0]; % relative expansion of wake zones
@@ -29,8 +29,8 @@ function [ model ] = floris_parameters( model )
         model.adjustInitialWakeDiamToYaw = false;
 
         % shear layer (only influences visualization)
-        model.shearCoefficientAlpha   = 0.10805;
-        model.shearZh                 = 90;
+        % model.shearCoefficientAlpha   = 0.10805;
+        % model.shearZh                 = 90;
         otherwise
             error(['Model parameters with name "' model.name '" not defined']);
     end;
