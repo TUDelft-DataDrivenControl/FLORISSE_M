d6933a4f5ae681963d2bf0fd8b664ef0585bf3bc
Bartdoekemeijer <B.M.Doekemeijer@tudelft.nl>
2017-07-03 12:04:35 +0200

    make demo code more user-friendly

--- demo_floris.m
+++ demo_floris.m
@@ -1,17 +1,26 @@
 clear all; close all; clc;
-% Optimize yaw angles
+%% Run a single simulation without optimization
+FLORIS = floris();          % Initialize FLORIS class. Default: floris('default','NREL5MW','9turb');
+FLORIS.run();               % Run a single simulation with the settings 'FLORIS.inputData'
+FLORIS.visualize(1,1,0);    % Calculate and display wind farm layout and top-view plot
+FLORIS.visualize(0,0,1);    % 3D visualization of the complete flow in the farm
+disp('Press a key to continue...'); pause;
+
+%% Optimize yaw angles
 FLORIS = floris();  % Initialize FLORIS class. Default: floris('default','NREL5MW','9turb');
 FLORIS.inputData.yawAngles = zeros(1,9);     % Set all turbines to greedy
 FLORIS.inputData.axialInd  = 0.33*ones(1,9); % Set all turbines to greedy
-FLORIS.optimize(true,false)                  % Optimization for yaw angles: same as .optimizeYaw()
+FLORIS.optimize(true,false);                 % Optimization for yaw angles: same as .optimizeYaw()
+disp('Press a key to continue...'); pause;
 
-% Optimize axial induction factor
+%% Optimize axial induction factor
 FLORIS = floris();  % Initialize FLORIS class. Default: floris('default','NREL5MW','9turb');
 FLORIS.inputData.yawAngles = zeros(1,9);     % Set all turbines to greedy
 FLORIS.inputData.axialInd  = 0.33*ones(1,9); % Set all turbines to greedy
 FLORIS.optimize(false,true);                 % Optimization for axial ind: same as .optimizeAxInd()
+disp('Press a key to continue...'); pause;
 
-% Optimize both axial induction and yaw
+%% Optimize both axial induction and yaw
 FLORIS = floris();  % Initialize FLORIS class. Default: floris('default','NREL5MW','9turb');
 FLORIS.inputData.yawAngles = zeros(1,9);     % Set all turbines to greedy
 FLORIS.inputData.axialInd  = 0.33*ones(1,9); % Set all turbines to greedy
