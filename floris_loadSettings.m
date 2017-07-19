487859dde67ce7fa4396d94f977a5af10d0aa264
Bart Doekemeijer <B.M.Doekemeijer@tudelft.nl>
2017-07-19 13:37:46 +0200

    Remove leftovers from gaussian wake model before merging with master branch

--- floris_loadSettings.m
+++ floris_loadSettings.m
@@ -28,9 +28,7 @@ end
               
 %% FLORIS model settings        
 switch lower(modelType)
-    case {'default','default_porteagel'}  %% original tuning parameters
-        inputData.wakeModel          = 'porteagel'; % Does nothing yet...
-        
+    case {'default'}  %% original tuning parameters
         % Choice of how a turbine's axial control setting is determined
         % 0: use pitch angles and Cp-Ct LUTs for pitch and WS, 
         % 1: greedy control   and Cp-Ct LUT for WS,
@@ -55,38 +53,11 @@ switch lower(modelType)
         inputData.aU                = 12.0; % units: degrees
         inputData.bU                = 1.3;
         
-        inputData.MU             = [0.5, 1.0, 5.5];
+        inputData.MU                = [0.5, 1.0, 5.5];
         
         % adjust initial wake diameter to yaw
         inputData.adjustInitialWakeDiamToYaw = false;        
-        
-%     case 'default_gebraad'  %% original tuning parameters
-%         inputData.wakeModel         = 'gebraad';
-%         inputData.pP                = 1.88; % yaw power correction parameter
-%         inputData.Ke                = 0.05; % wake expansion parameters
-%         % inputData.KeCorrArray     = 0.0; % array-correction factor: NOT YET IMPLEMENTED!
-%         inputData.KeCorrCT          = 0.0; % CT-correction factor
-%         inputData.baselineCT        = 4.0*(1.0/3.0)*(1.0-(1.0/3.0)); % Baseline CT for ke-correction
-%         inputData.me                = [-0.5, 0.22, 1.0]; % relative expansion of wake zones
-%         inputData.KdY               = 0.17; % Wake deflection recovery factor
-%         
-%         % define initial wake displacement and angle (not determined by yaw angle)
-%         inputData.useWakeAngle      = true;
-%         inputData.kd                = deg2rad(1.5);  % initialWakeAngle in X-Y plane
-%         inputData.ad                = -4.5; % initialWakeDisplacement
-%         inputData.bd                = -0.01;
-%         
-%         % correction recovery coefficients with yaw
-%         inputData.useaUbU           = true;
-%         inputData.aU                = 12.0; % units: degrees
-%         inputData.bU                = 1.3;
-%         
-%         inputData.MU               = [0.5, 1.0, 5.5];
-%         inputData.axialIndProvided = true;
-%         
-%         % adjust initial wake diameter to yaw
-%         inputData.adjustInitialWakeDiamToYaw = false;
-        
+                
     otherwise
         error(['Model type with name: "' modelType '" not defined']);
 end
@@ -110,7 +81,7 @@ switch lower(turbType)
             inputData.pitchAngles = zeros(1,nTurbs); % Set default as greedy
             inputData.axialInd    = zeros(1,nTurbs); % Create empty vector
             
-            % Determine Cp and Ct interpolation functions as functions of velocity
+            % Determine Cp and Ct interpolation functions as a function of WS and blade pitch
             for airfoilDataType = {'cp','ct'}
                 lut        = csvread([airfoilDataType{1} 'Pitch.csv']);
                 lut_ws     = lut(1,2:end);          % Wind speed in LUT in m/s
@@ -122,6 +93,8 @@ switch lower(turbType)
         elseif inputData.axialControlMethod == 1 % Greedy control with LUT for wind speed
             inputData.pitchAngles    = NaN*ones(1,nTurbs); % set blade pitch as NaN to avoid confusion
             inputData.axialInd       = zeros(1,nTurbs); % Create empty vector
+            
+            % Determine Cp and Ct interpolation functions as a function of WS
             lut                      = load('NREL5MWCPCT.mat');
             inputData.('cp_interp')  = @(ws) interp1(lut.NREL5MWCPCT.wind_speed,lut.NREL5MWCPCT.CP,ws);
             inputData.('ct_interp')  = @(ws) interp1(lut.NREL5MWCPCT.wind_speed,lut.NREL5MWCPCT.CT,ws);
