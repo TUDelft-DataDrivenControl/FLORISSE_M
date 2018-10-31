06330dba3424016963527cb5076eeeaf91b5df80
Bartdoekemeijer <B.M.Doekemeijer@tudelft.nl>
2018-10-31 11:56:25 +0100

    Update the test_dependencies function

--- Testing/test_dependencies.m
+++ Testing/test_dependencies.m
@@ -1,36 +1,31 @@
-function [] = test_dependencies(functionsPath)
-    fileNames = dir(functionsPath);
+function [] = test_dependencies(functionsPath,doRecursively)
 
-    % Functions folder
-    for j = 1:length(fileNames)-2
-        inputFile = fileNames(j+2).name;
+% Setup folders to look into
+if doRecursively
+    dirList = strsplit(genpath(functionsPath),';'); % Find directories recursively
+    dirList(strcmp(dirList,'')) = []; % Remove empty entries
+else
+    dirList = {functionsPath};
+end
 
-        % Check dependencies
-        [names{j}, folders{j}] = dependencies.toolboxDependencyAnalysis({inputFile});
-        disp(['Filename: ' inputFile '. Dependencies: ' names{j}])
+% Cycle through all folders
+for jdir = 1:length(dirList)
+    folderName = dirList{jdir};
+    fileNames = dir(folderName);
+    fileNames = fileNames(~[fileNames.isdir]); % Remove all directories
+    if length(fileNames) <= 0 && doRecursively == 0
+        error(['  ERROR: There are no files to inspect. Folder ''' folderName ''' exclusively has folders in it. Please enable doRecursively or specify a subdirectory manually.']);
     end
-    
-    % Functions/geomtryFunctions folder
-    fileNames = dir([functionsPath '/geometryFunctions']);
-    for j = 1:length(fileNames)-2
-        inputFile = fileNames(j+2).name;
-
+    for jFile = 1:length(fileNames)
+        inputFile = [fileNames(jFile).folder filesep fileNames(jFile).name];
+        
         % Check dependencies
-        [names{j}, folders{j}] = dependencies.toolboxDependencyAnalysis({inputFile});
-        disp(['Filename: ' inputFile '. Dependencies: ' names{j}])
-    end    
-
-    % Functions/visualizationFunctions folder
-    fileNames = dir([functionsPath '/visualizationFunctions']);
-    for j = 1:length(fileNames)-2
-        inputFile = fileNames(j+2).name;
-
-        % Check dependencies
-        [names{j}, folders{j}] = dependencies.toolboxDependencyAnalysis({inputFile});
-        disp(['Filename: ' inputFile '. Dependencies: ' names{j}])
-    end    
-    
-    % Make a compatibility report (only for newest MATLAB 2017b)
-    if exist('codeCompatibilityReport') ~= 0
-        codeCompatibilityReport(functionsPath);
+        [names, ~] = dependencies.toolboxDependencyAnalysis({inputFile});
+        disp(['Filename: ' folderName filesep fileNames(jFile).name '. Dependencies: ' strjoin(names,', ')])
     end
+end
+
+% Make a compatibility report (only for newest MATLAB 2017b)
+if exist('codeCompatibilityReport') ~= 0
+    codeCompatibilityReport(functionsPath);
+end
