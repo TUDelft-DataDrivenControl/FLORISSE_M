2c1d0dfb35c4516a7fff6f283d4326c93d17bd32
Bartdoekemeijer <B.M.Doekemeijer@tudelft.nl>
2018-11-02 11:12:52 +0100

    Add checks to ensure WD is in radians

--- FLORISSE_M/ambientFlowDefinitions/ambient_inflow_log.m
+++ FLORISSE_M/ambientFlowDefinitions/ambient_inflow_log.m
@@ -18,6 +18,7 @@ classdef ambient_inflow_log < ambient_inflow_interface
             %AMBIENT_INFLOW_LOG Construct an instance of this class
             obj.Vref = Vref;
             obj.Href = Href;
+            obj.check_angles_in_rad(windDirection);
             obj.windDirection = windDirection;
             obj.TI0 = TI0;
             obj.rho = 1.1716;
@@ -29,4 +30,15 @@ classdef ambient_inflow_log < ambient_inflow_interface
             V = obj.Vref*(Z/obj.Href).^alpha;
         end
     end
+    
+    methods (Access = protected)
+        function check_angles_in_rad(obj, x)
+            % check_angles_in_rad is a function that checks if an array with
+            % control settings only holds values in between -90 and +90
+            % degrees in rad
+            if any(x<-2*pi) || any(x>2*pi)
+                error('check_angles_in_rad:valueError', 'Wind direction must be specified in radians, angle>pi/2 || angle<pi/2');
+            end
+        end
+    end       
 end
--- FLORISSE_M/ambientFlowDefinitions/ambient_inflow_myfunc.m
+++ FLORISSE_M/ambientFlowDefinitions/ambient_inflow_myfunc.m
@@ -17,6 +17,7 @@ classdef ambient_inflow_myfunc < ambient_inflow_interface
                                              WDString, windDirection, ...
                                              TIString, TI0)
             %AMBIENT_INFLOW_LOG Construct an instance of this class
+            obj.check_angles_in_rad(windDirection);
             obj.windDirection = windDirection;
             obj.TI0    = TI0;
             obj.rho    = 1.1716;
@@ -29,4 +30,15 @@ classdef ambient_inflow_myfunc < ambient_inflow_interface
             V = reshape(obj.myFunc(Z(:)),size(Z));
         end
     end
+    
+    methods (Access = protected)
+        function check_angles_in_rad(obj, x)
+            % check_angles_in_rad is a function that checks if an array with
+            % control settings only holds values in between -90 and +90
+            % degrees in rad
+            if any(x<-2*pi) || any(x>2*pi)
+                error('check_angles_in_rad:valueError', 'Wind direction must be specified in radians, angle>pi/2 || angle<pi/2');
+            end
+        end
+    end        
 end
