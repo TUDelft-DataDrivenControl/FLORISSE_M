b55eb3361aca4456a78491461751dab1b22a3e2f
Bart Doekemeijer <B.M.Doekemeijer@tudelft.nl>
2018-12-06 11:52:33 +0100

    Add an option to minimize the error with a spatially-averaged section for LES calibration

--- FLORISSE_M/Optimizers/estimator.m
+++ FLORISSE_M/Optimizers/estimator.m
@@ -31,7 +31,7 @@ classdef estimator < handle
             disp(['Collective param. estimation set: [' strjoin(obj.estimParamsAll,', ') ']'])
         end
         
-        function [xopt,Jopt] = gaEstimation(obj,lb,ub)           
+        function [xopt,Jopt] = gaEstimation(obj,lb,ub)
             ga_A   = []; % Condition A * x <= b
             ga_b   = []; % Condition A * x <= b
             ga_Aeq = []; % Condition Aeq * x == beq
@@ -41,7 +41,7 @@ classdef estimator < handle
             
             % Optimize using Parallel Computing
             nVars = length(obj.estimParamsAll);
-%             options = gaoptimset('Display','iter', 'TolFun', 1e-3,'UseParallel', true); No plotting
+            %             options = gaoptimset('Display','iter', 'TolFun', 1e-3,'UseParallel', true); No plotting
             options = gaoptimset('Display','iter', 'TolFun', 1e-3,'UseParallel', true,'PlotFcns',{@plotfun1}); % with plot
             [xopt,Jopt,exitFlag,output,population,scores] = ga(costFun, nVars, ga_A, ga_b, ga_Aeq, ga_beq, lb, ub, [], options);
             
@@ -50,7 +50,7 @@ classdef estimator < handle
                 [~,idx]=min(state.Score);
                 optSettings=state.Population(idx,:);
                 bar(optSettings);
-                text(1:length(optSettings),optSettings,num2str(optSettings'),'vert','bottom','horiz','center'); 
+                text(1:length(optSettings),optSettings,num2str(optSettings'),'vert','bottom','horiz','center');
                 ylabel('Value');
                 xlabel('Estimation variables');
                 grid on; box on;
@@ -61,9 +61,9 @@ classdef estimator < handle
                 xlim([0 20]);
                 xlabel('Generation');
                 grid on; box on;
-               
-%                 set(gcf,'color','w');
-%                 export_fig(['estimationOutputs/kOut/' num2str(state.Generation) '.png'],'-m2');
+                
+                %                 set(gcf,'color','w');
+                %                 export_fig(['estimationOutputs/kOut/' num2str(state.Generation) '.png'],'-m2');
             end
         end
         
@@ -76,7 +76,7 @@ classdef estimator < handle
                 error('The variable [x] has to be of equal length as [estimationParams].');
             end
             
-            % Reset cost function 
+            % Reset cost function
             Jset = zeros(1,length(florisObjSet));
             
             % Update the parameters with [x] of each floris object, if required
@@ -97,7 +97,7 @@ classdef estimator < handle
                 % consistent yaw angles in the INERTIAL frame.
                 florisObjTmp.controlSet.yawAngleIFArray = florisObjTmp.controlSet.yawAngleIFArray;
                 
-
+                
                 % Execute FLORIS with [x]
                 florisObjTmp.run();
                 
@@ -111,8 +111,53 @@ classdef estimator < handle
                 if any(ismember(fields(measurementSet{i}),'U'))
                     fixYaw  = false;
                     uProbes = compute_probes(florisObjTmp,measurementSet{i}.U.x,measurementSet{i}.U.y,measurementSet{i}.U.z,fixYaw);
-                    flowError = uProbes - measurementSet{i}.U.values;
-                    Jset(i)   = Jset(i) + sqrt(mean((flowError ./ measurementSet{i}.U.stdev).^2));
+                    avgWSerror = uProbes - measurementSet{i}.U.values;
+                    Jset(i)   = Jset(i) + sqrt(mean((avgWSerror ./ measurementSet{i}.U.stdev).^2));
+                end
+                
+                % Calculate sector-averaged flow speed RMSE, if applicable
+                if any(ismember(fields(measurementSet{i}),'virtTurb'))
+                    fixYaw  = false;
+                    
+                    % Create flow field object
+                    flowFieldRes = measurementSet{i}.virtTurb.zPts(2)-measurementSet{i}.virtTurb.zPts(1);
+                    flowField = struct();
+                    [flowField.X, flowField.Y, flowField.Z] = meshgrid(...
+                        measurementSet{i}.virtTurb.x(:,1)', ...
+                        min(measurementSet{i}.virtTurb.y(:))-.75*measurementSet{i}.virtTurb.Drotor : flowFieldRes : max(measurementSet{i}.virtTurb.y(:))+.75*measurementSet{i}.virtTurb.Drotor, ...
+                        0 : flowFieldRes : max(measurementSet{i}.virtTurb.z(:))+.75*measurementSet{i}.virtTurb.Drotor);
+                                        
+                    % Set-up the FLORIS object, exporting the variables of interest
+                    layout               = florisObjTmp.layout;
+                    turbineResults       = florisObjTmp.turbineResults;
+                    yawAngles            = florisObjTmp.controlSet.yawAngleWFArray;
+                    avgWs                = [florisObjTmp.turbineConditions.avgWS];
+                    wakeCombinationModel = florisObjTmp.model.wakeCombinationModel;
+                    
+                    % Calculate flow field
+                    flowField.U = layout.ambientInflow.Vfun(flowField.Z);
+                    flowField = compute_flow_field(flowField, layout, turbineResults, ...
+                        yawAngles, avgWs, fixYaw, wakeCombinationModel);
+                    
+                    % Calculate UAvg for each virtual location  
+                    for ix = 1:size(flowField.X,2) % For each slice
+                        Y = squeeze(flowField.Y(:,ix,:));
+                        Z = squeeze(flowField.Z(:,ix,:));
+                        F = griddedInterpolant(Y,Z,squeeze(flowField.U(:,ix,:)));
+                        
+                        for iy = 1:size(measurementSet{i}.virtTurb.y,2)
+                            yVirtTurb = measurementSet{i}.virtTurb.y(ix,iy);
+                            yPts_tmp = yVirtTurb + measurementSet{i}.virtTurb.yPts;
+                            zPts_tmp = measurementSet{i}.virtTurb.z(ix,1) + measurementSet{i}.virtTurb.zPts;
+                            UAvg_floris(ix,iy) = mean(F(yPts_tmp,zPts_tmp));
+                        end
+                    end
+                    
+                    % Add  weighted cost per slice to overall cost function
+                    avgWSerror = UAvg_floris - measurementSet{i}.virtTurb.UAvg;
+                    for ix = 1:size(avgWSerror,1)
+                        Jset(i)   = Jset(i) + sqrt(mean((avgWSerror(ix,:) / measurementSet{i}.virtTurb.stdev(ix)).^2));
+                    end
                 end
             end
             
