dba0255547fbb2e01e63f4288a39285ede288a1d
Bartdoekemeijer <B.M.Doekemeijer@tudelft.nl>
2018-12-13 09:47:40 +0100

    Update default preprocessData case for the 10MW simulations

--- Examples/example_SOWFA_calibration/preprocessData.m
+++ Examples/example_SOWFA_calibration/preprocessData.m
@@ -2,17 +2,17 @@ clear all; close all; clc;
 addpath('bin')
 
 % Show time-averaged slices
-plotFigures = 'all'; % options: 'none', 'hor' (horizontal slice), 'all'
+plotFigures = 'none'; % options: 'none', 'hor' (horizontal slice), 'all'
 sampleStruct.xCutOff = [-Inf Inf]; % [-Inf Inf]
 sampleStruct.yCutOff = [-Inf Inf]; %[1000 2000];
 sampleStruct.zCutOff = [0 500];    % [-Inf Inf]
 
 % Flow sampling for downstream slices
-sampleStruct.sampleFlow = false;
-[sampleStruct.vertSlice_Y,sampleStruct.vertSlice_Z] = meshgrid(1000:25:2000,50:25:175);
+sampleStruct.sampleFlow = true;
+[sampleStruct.vertSlice_Y,sampleStruct.vertSlice_Z] = meshgrid(1000:25:2000,5:25:350);
 
 % Virtual turbine sampling
-sampleStruct.sampleVirtualTurbine = true;
+sampleStruct.sampleVirtualTurbine = false;
 sampleStruct.virtTurb.yRange = 1200:50:1800;
 sampleStruct.virtTurb.HH = 119.0; % hub height
 sampleStruct.virtTurb.Drotor = 178.3;
@@ -27,8 +27,14 @@ outputFiles = {};
 for yaw = [-30:10:30];
     % Post-processing folders, shell command:
     %  for i in {230..310..10}; do for j in {400..1000..5}; do mv piso_yaw$i/postProcessing/sliceDataInstantaneous/$j piso_yaw$i/postProcessing/sliceDataAvg/.; done; done
-    sourceFolders{end+1} = ['/home/bmdoekemeijer/OpenFOAM/bmdoekemeijer-2.4.0/simulationCases/WE2019/runs/piso_yaw' num2str(270-yaw) '/postProcessing/sliceDataAvg'];
-    outputFiles{end+1}   = ['./processedData/uniformInflow/yaw' num2str(yaw) '.mat'];
+    sourceFolders{end+1} = ['/home/bmdoekemeijer/OpenFOAM/bmdoekemeijer-2.4.0/simulationCases/WE2019/runs/piso_1turb_yaw' num2str(270-yaw) '/postProcessing/sliceDataAvg'];
+    outputFiles{end+1}   = ['./processedData/10MW_piso_1turb_yaw' num2str(yaw) '.mat'];
+end
+for yaw = [-30:10:30];
+    % Post-processing folders, shell command:
+    %  for i in {240..300..10}; do mkdir wps_yaw$i/postProcessing/sliceDataAvg; for j in {20400..21000..5}; do mv wps_yaw$i/postProcessing/sliceDataInstantaneous/$j wps_yaw$i/postProcessing/sliceDataAvg/.; done; done
+    sourceFolders{end+1} = ['/home/bmdoekemeijer/OpenFOAM/bmdoekemeijer-2.4.0/simulationCases/WE2019/runs/wps_1turb_lowTI_yaw' num2str(270-yaw) '/postProcessing/sliceDataAvg'];
+    outputFiles{end+1}   = ['./processedData/10MW_wps_1turb_lowTI_yaw' num2str(yaw) '.mat'];
 end
 
 % for i = 1:length(sourceFolders)
