379c350fb493f4b084cc365606e4e7ddc2eaeb92
Bartdoekemeijer <B.M.Doekemeijer@tudelft.nl>
2019-08-19 15:09:12 +0200

    Update LUT generation to include WD_std as an extra dimension

--- Examples/LUT_yawOptimization/generateYawLUT.m
+++ Examples/LUT_yawOptimization/generateYawLUT.m
@@ -13,19 +13,18 @@ addpath(genpath('../../FLORISSE_M'));
 % - - - - - - - - - - - - -  USER SET UP  - - - - - - - - - - - - - -  %
 databaseOutput = 'LUT_6turb_yaw.csv'; % Specify database output filename (do not forget '.mat' at the end)
 forceAppend    = false;    % Force write (skips safety check. Useful for HPC computations)
-WD_range = [0.:1.:90.]; % Span of wind directions (degrees)
-WS_range = [5.0:1.0:11.0];  % Span of wind speeds (m/s)
-TI_range = [0.02 0.07 0.12 0.17 0.25 0.35 0.50 0.90]; % Span of TIs (-)
+TI_range = 0.07 %[0.02 0.07 0.12];% 0.17 0.25 0.35 0.50 0.90]; % Span of TIs (-)
+WS_range = 8 %[5.0 8.0 11.0];  % Span of wind speeds (m/s)
+WD_range = 66 %[0.:2.:90.];%[-30.:2.:40.]; % Span of wind directions (degrees)
+WD_std_range = 2.5 %[0.0 2.5 5.0];% 10.]; % Standard deviation WD in degrees
 
-probablisticOptimization = false; % Optimize with uncertainty in WD
-WD_std  = 5.*pi/180; % Standard dev. in radians (if probablisticOptimization == 1)
-WD_N    = 5;         % N.o. sample points for prob. dist. (if probablisticOptimization == 1)
+WD_std_N    = 5;         % N.o. sample points for prob. dist.
 
 
 % - - - - - - - - - - - -  CORE OPERATIONS  - - - - - - - - - - - - -  %
 % Generate all combinations of WSs, WDs and TIs
-[X1,X2,X3] = ndgrid(WD_range,WS_range,TI_range);
-xTests  = [X1(:) X2(:) X3(:)]; % Generate test queue
+[X1,X2,X3,X4] = ndgrid(TI_range,WS_range,WD_range,WD_std_range);
+xTests  = [X1(:) X2(:) X3(:) X4(:)]; % Generate test queue
 
 % Load existing or initialize new database file
 if exist(databaseOutput,'file')
@@ -44,66 +43,62 @@ if exist(databaseOutput,'file')
 else
     noLinesInit = 1;
     disp(['Creating new database output file with name ''' databaseOutput '''.']);
-    %     dlmwrite(databaseOutput,sprintf('WD(deg) \t WS(m/s) \t TI (-) \t Pbl(W) \t Popt(W) \t xopt(deg)'),'delimiter','','newline','pc');
-    dlmwrite(databaseOutput,sprintf('WD(deg) \t WS(m/s) \t TI (-) \t Pbl (W) \t Popt \t xopt(deg, CW positive)'),'delimiter','','newline','pc');
+    dlmwrite(databaseOutput,sprintf('TI(-) \t WS(m/s) \t WD(deg) \t WD_std(deg) \t Pbl (W) \t Popt \t xopt(deg, CW positive)'),'delimiter','','newline','pc');
 end
 
 % Perform actual runs (in parallel, if possible)
 startTime = tic;
 N = size(xTests,1);
 disp(['Batch size: ' num2str(N) ' remaining optimization cases.']); disp(' ');
-if isunix % In our case: Linux HPC facility
+if isunix & isempty(gcp('nocreate')) % In our case: Linux HPC facility
     parpool(40)
 end
 parfor i = 1:N
 % for i = 1:N
-    disp(sprintf([datestr(rem(now,1)) ' __ Generating LUT entries for case: [%05.1f, %04.1f, %04.3f].'],xTests(i,1),xTests(i,2),xTests(i,3)));    
-    florisRunnerTmp = generateFLORISobject(xTests(i,1),xTests(i,2),xTests(i,3));
+    TItmp = xTests(i,1);
+    WStmp = xTests(i,2);
+    WDtmp = xTests(i,3);
+    WDstdtmp = xTests(i,4);
+    disp(sprintf([datestr(rem(now,1)) ' __ Generating LUT entries for case: [%04.3f, %04.3f, %04.3f, %04.3f].'],xTests(i,1),xTests(i,2),xTests(i,3),xTests(i,4)));    
+    florisRunnerTmp = generateFLORISobject(TItmp,WStmp,WDtmp); % Generate nominal object
     try
         
         % Choose which turbines to optimize (smart optimization)
-        if xTests(i,1) < 10 % Symmetry in the rows: T1 = T2, T3 = T4, T5 = T6 = 0
+        if WDtmp < 10 % Symmetry in the rows: T1 = T2, T3 = T4, T5 = T6 = 0
             turbsToOptimize = [1 3];
-        elseif xTests(i,1) < 20 % Only interaction with T1 on T6
+        elseif WDtmp >= 10 & WDtmp < 20 % Only interaction with T1 on T6
             turbsToOptimize = [1];
-        elseif xTests(i,1) < 45 % Only interaction with T1 and T3 on T4 and T6
+        elseif WDtmp >= 20 & WDtmp < 45 % Only interaction with T1 and T3 on T4 and T6
             turbsToOptimize = [1 3];
-        else
+        elseif WDtmp >= 45
             turbsToOptimize = [1]; % Symmetry in columns: T1 = T3 = T5, T2 = T4 = T6 = 0
         end
         
         % Determine optimal settings
-        if probablisticOptimization
-            % Robust optimization (over a prob. dist. of wind directions)
-            [xopt,Pbl,Popt] = optimizeControlSettingsRobustGS(florisRunnerTmp, ...
-                'Yaw Optimizer', 1, ...
-                'Pitch Optimizer', 0, ...
-                'Axial induction Optimizer', 0,...
-                WD_std, WD_N, ...
-                turbsToOptimize,... %
-                false); % silent execution
+        if WDstdtmp > 10*eps % Probablistic optimization
+            WD_std_rng_eval = WDstdtmp*pi/180; % convert to radians
+            WD_std_N_eval = WD_std_N;
         else
-            % Deterministic optimization (over a single wind direction)
-            [xopt,Pbl,Popt] = optimizeControlSettingsSimpleGS(florisRunnerTmp, ...
-                'Yaw Optimizer', 1, ...
-                'Pitch Optimizer', 0, ...
-                'Axial induction Optimizer', 0,...
-                turbsToOptimize, ... %
-                false); % silent execution
+            WD_std_rng_eval = 0.0001;
+            WD_std_N_eval = 1; % Evaluate one case
         end
+        % Robust optimization (over a prob. dist. of wind directions)
+        [xopt,Pbl,Popt] = optimizeControlSettingsRobustGS(florisRunnerTmp, ...
+            'Yaw', 1, 'Pitch', 0, 'Ax. induction', 0,...
+            WD_std_rng_eval, WD_std_N_eval, turbsToOptimize, false); % silent execution
 
         yawAnglesOpt = zeros(1,florisRunnerTmp.layout.nTurbs);
         yawAnglesOpt(turbsToOptimize) = xopt;
         
         % Implement symmetry
-        if xTests(i,1) < 10 % Symmetry in the rows: T1 = T2, T3 = T4, T5 = T6 = 0
+        if WDtmp < 10 % Symmetry in the rows: T1 = T2, T3 = T4, T5 = T6 = 0
             yawAnglesOpt(2) = yawAnglesOpt(1);
             yawAnglesOpt(4) = yawAnglesOpt(3);
-        elseif xTests(i,1) < 20 % Only interaction with T1 on T6
+        elseif WDtmp >= 10 & WDtmp < 20 % Only interaction with T1 on T6
             % do nothing, no symmetry exploited
-        elseif xTests(i,1) < 45 % Only interaction with T1 and T3 on T4 and T6
+        elseif WDtmp >= 20 & WDtmp < 45 % Only interaction with T1 and T3 on T4 and T6
             % do nothing, no symmetry exploited
-        else
+        elseif WDtmp >= 45
             yawAnglesOpt(3) = yawAnglesOpt(1);
             yawAnglesOpt(5) = yawAnglesOpt(1);
         end
@@ -117,8 +112,9 @@ parfor i = 1:N
 %         dlmwrite(databaseOutput,[xTests(i,:) Pbl Popt round(-xopt*180/pi,1)],'delimiter','\t','newline','pc','-append');
         dlmwrite(databaseOutput,[xTests(i,:) Pbl Popt round(yawAnglesOpt*180/pi,1)],'delimiter','\t','newline','pc','-append');
     catch
-        disp('Caught an error. Writing NaNs to this entry.')
-        dlmwrite(databaseOutput,[xTests(i,:) NaN],'delimiter','\t','newline','pc','-append');
+        disp('Caught an error. Not writing anything for this entry.')
+%         disp('Caught an error. Writing NaNs to this entry.')
+%         dlmwrite(databaseOutput,[xTests(i,:) NaN],'delimiter','\t','newline','pc','-append');
     end
 end
 disp([datestr(rem(now,1)) ' __ Finished ' num2str(N) ' runs. Terminating LUT generation.']);
@@ -136,7 +132,7 @@ fclose(fileID);
 end
 
 % Function to set-up FLORIS object
-function florisObj = generateFLORISobject(WD,WS,TI0)
+function florisObj = generateFLORISobject(TI0,WS,WD)
     layout = WE19_6_turb(); % Instantiate a layout without ambientInflow conditions
     refheight = layout.uniqueTurbineTypes(1).hubHeight; % Use the height from the first turbine type as reference height for theinflow profile
     layout.ambientInflow = ambient_inflow_log('PowerLawRefSpeed', WS,  'PowerLawRefHeight', refheight, ...
